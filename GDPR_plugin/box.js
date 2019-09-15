35655c5c3ef4f55508a5bf954c8f6fa3171d328c
Katarzyna Woznica <228105@student.pwr.edu.pl>
2019-09-15 19:25:49 +0200

    Accept and cancel buttons added

--- GDPR_plugin/box.js
+++ GDPR_plugin/box.js
@@ -9,34 +9,32 @@ export class Box {
     }
 
     create() {
-        let _createFadedOverlay = () => {
-            let overlay = document.createElement("div");
-            overlay.classList.add("box--overlay");
-            return overlay;
-        };
-
-
-        let _createModal = () => {
-            let modal = document.createElement("div");
-            modal.classList.add("box--modal");
-            modal.style.height = this.getHeight();
-            modal.style.width = this.getWidth();
-            let titleSection = document.createElement("section");
-            modal.appendChild(titleSection);
-            titleSection.appendChild(document.createElement("p").appendChild(document.createTextNode(this.title)));
-            titleSection.classList.add("box--modal-title");
-            modal.appendChild(document.createElement("hr"));
-
-            return modal;
-
-
-        };
-
-        let box = _createFadedOverlay();
-        box.appendChild(_createModal());
+        let box = this._createFadedOverlay();
+        box.appendChild(this._createModal());
 
         return box;
     }
+
+    open() {
+
+    }
+
+    _removeModal = () => {
+        let overlay = document.getElementById("overlay-id");
+        if (overlay) {
+            overlay.parentNode.removeChild(overlay);
+        }
+    };
+
+    _cancel = () => {
+        this._removeModal();
+        console.log("Cancel click");
+    };
+
+    _accept = () => {
+        this._removeModal();
+        console.log("Accept click");
+    };
 
     getHeight = () => {
         return this.height + "px";
@@ -46,16 +44,44 @@ export class Box {
         return this.width + "px";
     };
 
-    open() {
+    _createFadedOverlay = () => {
+        let overlay = document.createElement("div");
+        overlay.classList.add("box--overlay");
+        overlay.id = "overlay-id";
+        return overlay;
+    };
 
-    }
+    _createModal = () => {
+        let modal = document.createElement("div");
+        modal.classList.add("box--modal");
+        modal.style.height = this.getHeight();
+        modal.style.width = this.getWidth();
 
-    cancel() {
+        let titleSection = document.createElement("section");
+        modal.appendChild(titleSection);
+        titleSection.appendChild(document.createElement("p").appendChild(document.createTextNode(this.title)));
+        titleSection.classList.add("box--modal-title");
 
-    }
+        modal.appendChild(document.createElement("hr"));
 
-    accept() {
+        let contentSection = document.createElement("section");
+        modal.appendChild(contentSection);
+        contentSection.appendChild(document.createTextNode(this.content));
+        contentSection.classList.add("box--modal-content");
 
-    }
+        let buttonsSection = document.createElement("section");
+        modal.appendChild(buttonsSection);
+        let acceptButton = document.createElement("button");
+        let cancelButton = document.createElement("button");
+        buttonsSection.appendChild(acceptButton);
+        buttonsSection.appendChild(cancelButton);
+        acceptButton.appendChild(document.createTextNode("Ok"));
+        cancelButton.appendChild(document.createTextNode("Cancel"));
+
+        acceptButton.addEventListener("click", this._accept);
+        cancelButton.addEventListener("click", this._cancel);
+
+        return modal;
+    };
 
 }
--- GDPR_plugin/gdpr.js
+++ GDPR_plugin/gdpr.js
@@ -3,12 +3,4 @@ export class GDPR {
     constructor(approvalTimeLimit = 24) {
         this.approvalTimeLimit = approvalTimeLimit;
     }
-
-    // get approvalTimeLimit() {
-    //     return this._approvalTimeLimit;
-    // }
-    //
-    // set approvalTimeLimit(approvalTimeLimit) {
-    //     this._approvalTimeLimit = approvalTimeLimit;
-    // }
 }
