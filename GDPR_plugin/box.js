05f00054c489e8de4c7f8da427882052babcae7b
Katarzyna Woznica <228105@student.pwr.edu.pl>
2019-09-15 17:38:04 +0200

    added modal title

--- GDPR_plugin/box.js
+++ GDPR_plugin/box.js
@@ -9,17 +9,41 @@ export class Box {
     }
 
     create() {
-        let _createFadedBackground = () => {
-            let background = document.createElement("div");
-            background.classList.add("box--background");
-            return background;
+        let _createFadedOverlay = () => {
+            let overlay = document.createElement("div");
+            overlay.classList.add("box--overlay");
+            return overlay;
         };
-        return _createFadedBackground();
 
+
+        let _createModal = () => {
+            let modal = document.createElement("div");
+            modal.classList.add("box--modal");
+            modal.style.height = this.getHeight();
+            modal.style.width = this.getWidth();
+            let titleSection = document.createElement("section");
+            modal.appendChild(titleSection);
+            titleSection.appendChild(document.createElement("p").appendChild(document.createTextNode(this.title)));
+            titleSection.classList.add("box--modal-title");
+            modal.appendChild(document.createElement("hr"));
+
+            return modal;
+
+
+        };
+
+        let box = _createFadedOverlay();
+        box.appendChild(_createModal());
+
+        return box;
     }
 
     getHeight = () => {
         return this.height + "px";
+    };
+
+    getWidth = () => {
+        return this.width + "px";
     };
 
     open() {
